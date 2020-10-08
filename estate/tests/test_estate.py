b92cd63d6431f259087e6f3ef6b05842f5eac8a0
william-andre <wan@odoo.com>
2020-10-08 18:12:33 +0200

    Add solution for the Python Unit Tests training

---
+++ estate/tests/test_estate.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+from odoo.tests.common import SavepointCase
+from odoo.exceptions import UserError
+from odoo.tests import tagged
+from odoo.tests.common import Form
+
+
+@tagged('post_install', '-at_install')
+class EstateTestCase(SavepointCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super(EstateTestCase, cls).setUpClass()
+
+        cls.buyer = cls.env['res.partner'].create({
+            'name': 'buyer',
+        })
+        cls.properties = cls.env['estate.property'].create([{
+            'name': 'prop1',
+            'expected_price': 10000,
+        }])
+        cls.offers = cls.env['estate.property.offer'].create([{
+            'partner_id': cls.buyer.id,
+            'property_id': cls.properties[0].id,
+            'price': 9000,
+        }])
+
+    def test_action_sell(self):
+        """Test that everything behaves like it should when selling a property."""
+        # You cannot sell a property without an accepted offer
+        with self.assertRaises(UserError):
+            self.properties.action_sold()
+
+        # accept the offer
+        self.offers.action_accept()
+
+        # Now you can sell it
+        self.properties.action_sold()
+        self.assertRecordValues(self.properties, [
+           {'state': 'sold'},
+        ])
+
+        # You cannot create an offer for a sold property
+        with self.assertRaises(UserError):
+            self.env['estate.property.offer'].create([{
+                'partner_id': self.buyer.id,
+                'property_id': self.properties[0].id,
+                'price': 10000,
+            }])
+
+    def test_property_form(self):
+        """Test the form view of properties."""
+        with Form(self.properties[0]) as prop:
+            self.assertEqual(prop.garden_area, 0)
+            self.assertIs(prop.garden_orientation, False)
+            prop.garden = True
+            self.assertEqual(prop.garden_area, 10)
+            self.assertEqual(prop.garden_orientation, "N")
+            prop.garden = False
+            self.assertEqual(prop.garden_area, 0)
+            self.assertIs(prop.garden_orientation, False)
