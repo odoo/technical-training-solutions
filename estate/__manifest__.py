049f5937434d9c147c204dd1a97bd6547c528ac7
Tiffany Chang (tic) <tic@odoo.com>
2021-04-20 10:59:41 +0200

    Add solution for dashboard training

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -6,6 +6,7 @@
     "depends": [
         "base",
         "web",
+        "web_dashboard"
     ],
     "data": [
         "security/ir.model.access.csv",
@@ -15,6 +16,10 @@
         "views/estate_property_views.xml",
         "views/res_users_views.xml",
         "views/estate_menus.xml",
+        "report/estate_report_views.xml",
+    ],
+    "demo": [
+        "data/estate_demo.xml"
     ],
     "application": True,
 }
